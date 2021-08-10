102fce148c9c401988d65625dddfe19745dc7058
msyed92 <84087360+msyed92@users.noreply.github.com>
2021-08-09 20:02:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,3 @@ $("button").click(function () {
     $(".toggle").toggleClass("remove");
   }
 });
-
-$(window).resize(function () {
-  if ($(".sidebar").hasClass("show-sidebar") && $(window).width() < 601) {
-    $(".toggle").addClass("remove");
-  }
-});
